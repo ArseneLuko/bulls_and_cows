6ca34ff6926f4ff7bd455b06f92fd800d99adb18
ArseneLuko <arsene.luko@gmail.com>
2024-08-10 22:50:39 +0200

    založení ukládání

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -41,7 +41,7 @@ def vypis_statistiky(pokusy, cas):
     vypis_radek(hlaseni["pokusy"].format(pokusy))
     vypis_radek(hlaseni["cas"].format(cas))
     vypis_radek(hlaseni["prumer"].format(round(cas / pokusy, 1)))
-    vypis_radek(opakovani=2)
+    vypis_radek()
 
 def vytvor_hadane_cislo(velikost: int) -> str:
     """
@@ -172,7 +172,7 @@ if __name__ == "__main__":
 
     # vytvoří hádané číslo v délce uživatelského vstupu
     hadane_cislo = vytvor_hadane_cislo(velikost_cisla := zadej_delku_cisla())
-    # vypis_radek(hadane_cislo, "stred") # debugování, vypíše číslo
+    vypis_radek(hadane_cislo, "stred") # debugování, vypíše číslo
     
     vypis_radek(hlaseni["generovano"].format(velikost_cisla))
     vypis_radek(hlaseni["mereni_casu"])
